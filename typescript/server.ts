1ff6fec20f6b153e755478343be3522c79f49c58
georgesycabaccang <georgesycabaccang@gmail.com>
2023-04-24 16:20:15 +0800

    Add app.options(*, cors()) for CORS error

--- typescript/server.ts
+++ typescript/server.ts
@@ -25,9 +25,10 @@ db.once("open", () => console.log("Connected to DB."));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
-app.use(cors());
+// app.use(cors());
+app.options("*", cors());
 
-app.use("/", moviesRoutes);
+app.use("/movies", moviesRoutes);
 
 app.listen(env.PORT, () => console.log(`Listeninig to Port ${env.PORT}`));
 
