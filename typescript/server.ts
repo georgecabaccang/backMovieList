1327601d1573965713882046eb9fb5b0d4e1a29b
georgesycabaccang <georgesycabaccang@gmail.com>
2023-04-24 16:59:49 +0800

    Rest

--- typescript/server.ts
+++ typescript/server.ts
@@ -1,7 +1,6 @@
 import { load } from "ts-dotenv";
 
 import express from "express";
-import ServerlessHttp from "serverless-http";
 import mongoose from "mongoose";
 import cors from "cors";
 
@@ -25,11 +24,10 @@ db.once("open", () => console.log("Connected to DB."));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
-app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173"] }));
+app.use(cors());
 
 app.use("/movies", moviesRoutes);
 
 app.listen(env.PORT, () => console.log(`Listeninig to Port ${env.PORT}`));
 
-module.exports.hanlder = ServerlessHttp(app);
 export = {};
