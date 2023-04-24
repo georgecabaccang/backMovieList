0d99d42e85d199f81fd633cf8a5e8cd532471626
georgesycabaccang <georgesycabaccang@gmail.com>
2023-04-24 14:57:55 +0800

    Add headers in netlify.toml for cors access

--- typescript/server.ts
+++ typescript/server.ts
@@ -27,7 +27,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-app.use("/.netlify/functions/routes/moviesRoutes", moviesRoutes);
+app.use("/movies", moviesRoutes);
 
 app.listen(env.PORT, () => console.log(`Listeninig to Port ${env.PORT}`));
 
