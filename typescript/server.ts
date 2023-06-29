9431806ee1735bb9c29da7cb1532423152520246
georgesycabaccang <georgesycabaccang@gmail.com>
2023-06-29 19:51:13 +0800

    What now?

--- typescript/server.ts
+++ typescript/server.ts
@@ -25,7 +25,7 @@ db.once("open", () => console.log("Connected to DB."));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
-app.use(cors({origin: "https://front-movie-list.vercel.app"}));
+app.use(cors({ origin: "*" }));
 // app.use(cors());
 
 app.use("/movies", moviesRoutes);
