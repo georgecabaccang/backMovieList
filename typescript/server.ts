e1b167d851e3192bf34b1ec71e28285a1e44a951
georgesycabaccang <georgesycabaccang@gmail.com>
2023-06-29 23:08:28 +0800

    Try to add in server.ts line 23 to 27

--- typescript/server.ts
+++ typescript/server.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
@@ -19,7 +19,12 @@ db.once("open", () => console.log("Connected to DB."));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
-app.use(cors({ credentials: true, origin: "https://front-movie-list.vercel.app" }));
+// app.use(cors({ origin: "https://front-movie-list.vercel.app" }));
+app.use(function (req: Request, res: Response, next: NextFunction) {
+    res.header("Access-Control-Allow-Origin", "https://front-movie-list.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/movies", moviesRoutes);
 
